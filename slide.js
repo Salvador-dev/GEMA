1cebc3804f7584986a13bddbfc310d58ba2fd00f
Salvador-dev <83884209+Salvador-dev@users.noreply.github.com>
2021-09-14 13:50:01 -0400

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,15 @@
+const text = document.querySelector(".text");
+text.innerHTML = text.textContent.replace(/\S/g,
+    "<span>$&</span>");
+
+    const element = document.querySelectorAll("span");
+    for(let i = 0; i<element.length; i++){
+        element[i].style.transform = "rotate("+i*18+"deg) "
+        
+    }
+
+    document.addEventListener('scroll', e=>{
+        document.querySelector('.text').style.transform = `rotate(-${80+scrollY/3}deg)`
+
+    })
+    
